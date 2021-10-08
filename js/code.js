38c1a1ef7ff204d84863d65eab465bf3297ed88b
blum <alfredoblumtorres@gmail.com>
2021-10-08 19:51:26 +0200

    Second commit

--- js/code.js
+++ js/code.js
@@ -1,3 +1,8 @@
 function calculo() {
-    alert("Hellow");
+    //alert("Hellow");
+    let totalCuenta = document.getElementById('totalCuenta').value; //Recogemos la variable enviada desde html, toda la sintáxis es necesaria, si no especificamos algo como el value, extrae todo
+    let servicio = document.getElementById('servicio').value; //Conjunto de elementos, nos da un array, y hay que seleccionar uno
+    // Traer el valor de nº de personas mediante la clase asociada
+    let personas = document.getElementById('personas').value;
+    console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas);
 }
