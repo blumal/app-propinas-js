94cd071afaec3ceb3188e1e4c38a3402fb3fbbfc
blum <alfredoblumtorres@gmail.com>
2021-10-11 19:39:40 +0200

    Calculadora finalizada con js

--- js/code.js
+++ js/code.js
@@ -3,6 +3,23 @@ function calculo() {
     let totalCuenta = document.getElementById('totalCuenta').value; //Recogemos la variable enviada desde html, toda la sintáxis es necesaria, si no especificamos algo como el value, extrae todo
     let servicio = document.getElementById('servicio').value; //Conjunto de elementos, nos da un array, y hay que seleccionar uno
     // Traer el valor de nº de personas mediante la clase asociada
-    let personas = document.getElementById('personas').value;
-    console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas);
+    //let personas = document.getElementsByClassName('personas')[0].value;
+    let personas = document.getElementById('personas').value; //Value solo para inputs
+
+    let res = (servicio * totalCuenta) / personas;
+    if (isNaN(res)) {
+        alert('Introduce datos válidos');
+    } else if (personas == '' || personas == 0) {
+        alert('Introduce las personas que van a pagar');
+    } else {
+        document.getElementById('res').innerHTML = '<p>Total a pagar</p><h3> ' + res + ' Por persona<h3>'; //Trae el contenido de ese elemento (innerHTML), y en este caso cámbialo por res
+    }
+
+}
+
+//Funcion para resetear el formulario
+function limpiar() {
+    document.getElementById('res').innerHTML = ""; //Recogo todo el id de html y lo convierto a vacío, si no me seguirá mostrando el resultado de la operación
+    document.getElementById('totalCuenta').value = "";
+    document.getElementById('personas').value = "";
 }
